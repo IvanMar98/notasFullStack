b695dd776c99f6e6b602039d7913a477519254e6
Ivan Martinez <201720533@tese.edu.mx>
2023-07-30 00:43:41 -0500

    cambiar estructura del objeto de las notas y cambiar de api

--- src/services/notes/getAllNotes.js
+++ src/services/notes/getAllNotes.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getAllNotes = () => {
-    return axios.get('https://jsonplaceholder.typicode.com/posts')
+    return axios.get('https://notas-backend-production.up.railway.app/notes')
     .then((response) => {
         const { data } = response;
         return data;
--- src/services/notes/postNote.js
+++ src/services/notes/postNote.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const postNewNote = ({ title, body, userId}) => {
+export const postNewNote = ({ date, content, important}) => {
     return axios
-        .post('https://jsonplaceholder.typicode.com/posts', { title, body, userId })
+        .post('https://notas-backend-production.up.railway.app/notes', { date, content, important})
         .then((response) => {
             const { data } = response;
             return data;
